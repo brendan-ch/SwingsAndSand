b3e4594f0fc57eb07940dd6902ca67f69cdf912b
Brendan Chen <me@bchen.dev>
2024-12-16 19:51:30 -0800

    Initial Commit

---
+++ SwingsAndSand/SwingsAndSandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingsAndSandApp.swift
+//  SwingsAndSand
+//
+//  Created by Brendan Chen on 2024.12.16.
+//
+
+import SwiftUI
+
+@main
+struct SwingsAndSandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
